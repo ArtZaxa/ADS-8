5cd049f7cf788a14185ac72be37f0f1d02700be9
Lubitel <zaharushkinartem07@mail.ru>
2026-05-28 12:47:32 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
 // Copyright 2021 NNTU-CS
 #include "bst.h"
 
-int main() {
+void makeTree(BST<std::string>& tree, const char* filename);
+void printFreq(BST<std::string>& tree);
+
+int main()
+{
+    const char* filename = "war_peace.txt";
+    BST<std::string> tree;
+    makeTree(tree, filename);
+    int depth = tree.depth();
+    std::cout << (depth == 35) << " " << tree.search("andrew");
+
+    printFreq(tree);
+
     return 0;
 }
